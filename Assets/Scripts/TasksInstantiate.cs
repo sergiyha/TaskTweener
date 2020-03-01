d8c372fc228c3116c84ac0a164816573800c95db
s.halanov <s,halanov@lucidrealitylabs.com>
2020-03-02 01:28:23 +0200

    init Comit
    initial

---
+++ Assets/Scripts/TasksInstantiate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class TasksInstantiate : MonoBehaviour
+{
+	public GameObject Object;
+	private Task<string> _task;
+	public float Duration;
+	public float ScaleFactor;
+	private ITaskTweener _tweener;
+	private ITaskTweener _tweener_2;
+
+	public Vector3 speed;
+
+	private void Start()
+	{
+		
+		EasingFunction.Ease ease = EasingFunction.Ease.EaseInOutQuad;
+
+
+		var localScale = Object.transform.localScale;
+		_tweener = Object.transform.Scale(localScale,
+			new Vector3(localScale.x * ScaleFactor, localScale.y * ScaleFactor, localScale.z * ScaleFactor), Duration);
+		_tweener_2 = Object.transform.Scale(
+			new Vector3(localScale.x * ScaleFactor, localScale.y * ScaleFactor, localScale.z * ScaleFactor),
+			localScale,
+			Duration);
+
+
+		// _tweener_2 = new TaskTweener(
+		// 	Object.GetComponent<Transform>(),
+		// 	new Vector3(localScale.x * ScaleFactor, localScale.y * ScaleFactor, localScale.z * ScaleFactor),
+		// 	localScale,
+		// 	Duration,
+		// 	(v1, v2, l) => Vector3.Lerp(v1, v2, l),
+		// 	Object.GetComponent<Transform>().ScaleAction(),
+		// 	() => Debug.LogError("Completed 1"),
+		// 	() => Debug.LogError("Canceled 1"));
+
+		Flow();
+		//Stop();
+	}
+
+	async Task Flow()
+	{
+		await Task.Delay(2000);
+		await _tweener.Start();
+		await _tweener_2.Start();
+	}
+
+	async Task Stop()
+	{
+		await Task.Delay(2000);
+		_tweener.Cancel();
+	}
+
+	private void Update()
+	{
+		var rotation = Object.transform.rotation;
+		Object.transform.Rotate(new Vector3(speed.x, speed.y, speed.z));
+	}
+}
+
---
+++ Assets/Scripts/TweenExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public static class TweenExtension
+{
+	#region Transform
+
+	public static ITaskTweener Scale(this Transform tr, Vector3 from, Vector3 to, float duration)
+	{
+		return new TaskTweener<Vector3>(from, to, duration, FunctionsLibrary.Linear(), tr.ScaleAction());
+	}
+
+	public static ITaskTweener LocalPosition(this Transform tr, Vector3 from, Vector3 to, float duration)
+	{
+		return new TaskTweener<Vector3>(from, to, duration, FunctionsLibrary.Linear(), tr.LocalPositionAction());
+	}
+
+	public static ITaskTweener Position(this Transform tr, Vector3 from, Vector3 to, float duration)
+	{
+		return new TaskTweener<Vector3>(from, to, duration, FunctionsLibrary.Linear(), tr.PositionAction());
+	}
+
+	#endregion
+}
+
+public static class ActionsLibrary
+{
+	public static Action<Vector3> ScaleAction(this Transform transform)
+	{
+		return (Vector3 updatedScale) => { transform.localScale = updatedScale; };
+	}
+
+	public static Action<Vector3> LocalPositionAction(this Transform transform)
+	{
+		return (Vector3 updatedLocalPosition) => { transform.localPosition = updatedLocalPosition; };
+	}
+
+	public static Action<Vector3> PositionAction(this Transform transform)
+	{
+		return (Vector3 updatedPosition) => { transform.position = updatedPosition; };
+	}
+}
+
+public static class FunctionsLibrary
+{
+	public static Func<Vector3, Vector3, float, Vector3> Linear()
+	{
+		return (Vector3 s, Vector3 f, float value ) => new Vector3(
+			EasingFunction.Linear(s.x,f.x, value),
+			EasingFunction.Linear(s.y,f.y, value),
+			EasingFunction.Linear(s.z,f.z, value));
+	}
+	
+	public static Func<float, float, float, float> LinearFloat()
+	{
+		return EasingFunction.Linear;
+	}
+}
